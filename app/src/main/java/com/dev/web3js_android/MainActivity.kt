1b520259c8f096cd644c7423c65b0321e412def0
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2021-11-30 00:01:00 +0100

    Initial commit

---
+++ app/src/main/java/com/dev/web3js_android/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.dev.web3js_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.WalletUtils
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.protocol.core.methods.response.EthGetBalance
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.protocol.core.methods.response.Web3ClientVersion
+import org.web3j.protocol.http.HttpService
+import org.web3j.tx.Transfer
+import org.web3j.utils.Convert
+import java.io.File
+import java.lang.Exception
+import java.math.BigDecimal
+import java.security.Security
+
+class MainActivity : AppCompatActivity() {
+    var web3: Web3j? = null
+    var file: File? = null
+    var Walletname: String? = null
+    var credentials: Credentials? = null
+    var txtaddress: TextView? = null
+    val main: Mainviewmodel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtaddress = findViewById(R.id.text_address)
+
+        //enter your own infura api key below
+        //  web3 = Web3j.build(HttpService("https://rinkeby.infura.io/v3/e344d511c77a4f78af0880dc3e90ca7b"))
+
+        // setupBouncyCastle()
+
+        //  this is the pathname for the file that will be created and stores the wallet details
+        val Edtpath = findViewById<EditText>(R.id.walletpath)
+        val etheriumwalletPath = Edtpath.text.toString()
+        val Edtpassword = findViewById<EditText>(R.id.password)
+        val txtbalance = findViewById<TextView>(R.id.text_balance)
+        val btncnt = findViewById<Button>(R.id.buttonsconnect)
+        val btnblnc = findViewById<Button>(R.id.buttonbalance)
+        val creatbtnb = findViewById<Button>(R.id.create_button)
+        val sendethbtn = findViewById<Button>(R.id.sendEth)
+        val Edtvalue = findViewById<EditText>(R.id.ethvalue)
+
+        file = File(filesDir.toString() + etheriumwalletPath) // the etherium wallet location
+
+        //create the directory if it does not exist
+        if (!file!!.mkdirs()) {
+            file!!.mkdirs()
+        } else {
+            Toast.makeText(
+                applicationContext, "Directory already created",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        main.setupBouncyCastle()
+        main.getbalance.observe(this, Observer {
+            txtbalance.text = it
+        })
+        btncnt.setOnClickListener {
+            // connect to the etheruem network
+            main.connectToEthNetwork(this)
+        }
+        btnblnc.setOnClickListener {
+            main.retrieveBalance(this)
+        }
+        creatbtnb.setOnClickListener {
+            // Create wallet
+            main.createWallet(Edtpassword.text.toString(), txtaddress, file, this)
+        }
+        sendethbtn.setOnClickListener {
+            main.makeTransaction(Edtvalue.text.toString().toInt(), this)
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/dev/web3js_android/Viewmodel.kt
@@ -0,0 +1,167 @@
+package com.dev.web3js_android
+
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.res.TypedArrayUtils.getString
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.WalletUtils
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.protocol.core.methods.response.EthGetBalance
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.protocol.core.methods.response.Web3ClientVersion
+import org.web3j.protocol.http.HttpService
+import org.web3j.tx.Transfer
+import org.web3j.utils.Convert
+import java.io.File
+import java.lang.Exception
+import java.math.BigDecimal
+import java.security.Security
+
+class Mainviewmodel : ViewModel() {
+    var web3: Web3j? = null
+
+    //  var file: File? = null
+    var Walletname: String? = null
+    var credentials: Credentials? = null
+    val getbalance: MutableLiveData<String> = MutableLiveData()
+
+    //enter your own infura api key below
+    init {
+        web3 =
+            Web3j.build(HttpService("https://rinkeby.infura.io/v3/e344d511c77a4f78af0880dc3e90ca7b"))
+
+    }
+
+
+    fun connectToEthNetwork(context: Context) {
+        viewModelScope.launch {
+            withContext(Dispatchers.Main){
+                Toast.makeText(context, "Now Connecting to Ethereum network", Toast.LENGTH_LONG).show()
+
+                try {
+                    //if the client version has an error the user will not gain access if successful the user will get connnected
+                    val clientVersion: Web3ClientVersion? =
+                        web3?.web3ClientVersion()?.sendAsync()?.get()
+                    if (!clientVersion?.hasError()!!) {
+
+                        Toast.makeText(context, "Connected!", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(context, clientVersion.error.message, Toast.LENGTH_LONG).show()
+
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    fun retrieveBalance( context: Context) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO){
+                //get wallet's balance
+                try {
+                    val balanceWei: EthGetBalance? = web3?.ethGetBalance(
+                        credentials?.address,
+                        DefaultBlockParameterName.LATEST
+                    )?.sendAsync()
+                        ?.get()
+                  //  txtbalance?.text = "Your Balance" +
+                            getbalance.postValue(balanceWei?.balance.toString()) //balanceWei?.balance
+                } catch (e: Exception) {
+                    Toast.makeText(context, "balance failed", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+
+    }
+
+    fun createWallet(edtpassword: String, txtaddress: TextView?, file: File?, context: Context) {
+        viewModelScope.launch {
+            withContext(Dispatchers.Main){
+                val password = edtpassword // this will be your etherium password
+                try {
+                    // generating the etherium wallet
+                    Walletname = WalletUtils.generateLightNewWalletFile(password, file)
+                    Toast.makeText(context, "Wallet generated wallet name is", Toast.LENGTH_LONG).show()
+
+                    credentials =
+                        WalletUtils.loadCredentials(password, file.toString() + "/" + Walletname)
+                    txtaddress?.text = credentials?.address
+                    //  ShowToast("success" +  credentials?.address)
+                } catch (e: Exception) {
+                    Toast.makeText(context, "failed", Toast.LENGTH_LONG).show()
+
+                }
+            }
+
+        }
+
+    }
+
+    fun makeTransaction(edtvalue: Int, context: Context) {
+        viewModelScope.launch {
+            withContext(Dispatchers.Main){
+                val value = edtvalue
+                try {
+                    val receipt: TransactionReceipt = Transfer.sendFunds(
+                        web3,
+                        credentials,
+                        credentials?.address,
+                        BigDecimal.valueOf(value.toLong()),
+                        Convert.Unit.ETHER
+                    ).send()
+                    Toast.makeText(
+                        context,
+                        "Transaction successful: " + receipt.transactionHash,
+                        Toast.LENGTH_LONG
+                    ).show()
+                } catch (e: Exception) {
+
+                    Log.d("low_balance", "low balance")
+                }
+            }
+
+        }
+
+    }
+
+    //set up the security provider
+    fun setupBouncyCastle() {
+        viewModelScope.launch {
+            withContext(Dispatchers.Main){
+                val provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)
+                    ?: // Web3j will set up a provider  when it's used for the first time.
+                    return@withContext
+                if (provider.javaClass == BouncyCastleProvider::class.java) {
+                    return@withContext
+                }
+                //There is a possibility  the bouncy castle registered by android may not have all ciphers
+                //so we  substitute with the one bundled in the app.
+                Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+                Security.insertProviderAt(BouncyCastleProvider(), 1)
+            }
+
+        }
+
+    }
+
+}
